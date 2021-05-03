27fe42cf59a8547fb19b5205df2a95c2de118500
Georgi768 <59054341+Georgi768@users.noreply.github.com>
2021-05-03 11:59:48 +0200

    created project

---
+++ src/main.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    myWord = 'Hello World'
+
+
+    def isHelloWorld(word):
+        if word == "Hello World":
+            return True
+
+    print(isHelloWorld(myWord))
